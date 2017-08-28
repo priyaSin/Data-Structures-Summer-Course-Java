4c7663d1d9b3f701e026e6145e026a2593070119
Priya Singh <priyaSin@users.noreply.github.com>
2017-08-28 13:22:50 +0530

    Assignment 2b completed

--- Ques5a.java
+++ Ques5a.java
@@ -4,15 +4,40 @@ Problem Statement - Write a program to find square root of an input.
 				a. Just find the integral part
 				b. Find the square root with an accuracy of n decimal
 				points, n is provided by the user.
+IDEA - Idea is to use Babylion method in both the cases.
 
 */
 
 import java.util.Scanner;
-public class Ques5{
+public class Ques5a{
+	public static int findSquareRoot(int num ){
+		//Taking a estimate of the rootRoot
+		int value = num;
+		int noOfDigits = 0;
+		while(value > 0){
+			value = value / 10;
+			noOfDigits++;
+		}
+		System.out.println("The number of digits in the number is " + noOfDigits + "\n" 
+			+ "So the estimated number will be smallest number of half the number of digits than " + num);
+		// Estimate value (x) and num/x
+		double x = Math.pow(10 , noOfDigits / 2);
+		double x1 = num / x;
+
+		for(int i = 1 ; i <= 10 ; i++ ){
+		// new x is arithematic mean of x and x1 and accordingly x1 is modified
+		x = (x + x1)/2;
+		x1 = num/x ;
+		}
+		return (int) x ;
+
+	}
 	public static void main(String [] args){
-		System.out.println("Enter a number to find square root:")
+		System.out.println("Enter a number to find square root:");
 		Scanner s = new Scanner(System.in);
 		int num = s.nextInt();
-		
+		int sqRoot = findSquareRoot(num );
+		System.out.println("The value of square root of " + num + " is  " + sqRoot);
+
 	}
 }
---
+++ Ques6.java
@@ -0,0 +1,55 @@
+/* ASSIGNMENT - 2b QUESTION-6 
+
+Problem Statement - You are given S a sequence of n integers S = s1,s2, ..., sn.
+Please, compute if it is possible to split S into two parts : s1, s2, ..., si and si+1,
+si+2, ....., sn (1 <= i < n) in such a way that the first part is strictly decreasing while the
+second is strictly increasing one. First take n as input and then take n more integers, output yes
+or no.
+
+*/
+import java.util.Scanner;
+public class Ques6{
+
+	public static Boolean detectSequence(int [] arr){
+		Boolean flag = true;
+		int count = 0;
+		int a [] = new int[arr.length];
+		int j = 0;
+		for(int i = 0 ; i < arr.length-1 ; i++){
+			if(arr [i] < arr[i+1]){
+				count++;
+				a[j] = i;
+				j++;
+			}
+		}
+		System.out.println(count);
+		if(count == arr.length-a[0]-1){
+			flag = true;
+		}
+		else{
+			flag = false;
+		}
+		return flag;
+
+
+	}
+
+
+	public static void main(String [] args){
+		System.out.println("Enter the value of n:");
+		Scanner s = new Scanner (System.in);
+		int n = s.nextInt();
+		int [] arr = new int[n];
+		System.out.println("Enter n integers:");
+		for(int i = 0 ; i < n ; i++){
+			arr[i] = s.nextInt();
+		}
+		Boolean flag = detectSequence(arr);
+		if(flag == true){
+		System.out.println("Yes");
+	    }
+	    else{
+	    	System.out.println("No");
+	    }
+	}
+}
